9524039b6fa01e9267350060cef4ac3207d28ae0
Nur Intan Alatas <intannnalatas@gmail.com>
2020-09-12 21:41:41 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router } from 'svelte-routing'
+  import Route from './routes/Route.svelte'
+  import Navbar from './components/Navbar.svelte'
+
+  export let url
+</script>
+
+<Router {url}>
+  <Navbar />
+  <Route />
+</Router>
---
+++ src/routes/Route.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Route } from 'svelte-routing'
+  import Home from '../pages/Home.svelte'
+  import About from '../pages/About.svelte'
+</script>
+
+<div>
+  <Route path="about" component={About} />
+  <Route path="/">
+    <Home name="World" />
+  </Route>
+</div>
